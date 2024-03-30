b7b5e3a643ba9c3bc9b90512fbb78331bb0570e0
fizmahmood <40705531+fizmahmood@users.noreply.github.com>
2024-03-30 19:28:05 +0000

    Add files via upload
    Added Logger

---
+++ logging/LogLevel.java
@@ -0,0 +1,9 @@
+package com.airline.logging;
+
+// Enumeration representing different log levels
+public enum LogLevel {
+    INFO,     // Informational messages
+    WARNING,  // Warnings
+    ERROR,    // Errors
+    DEBUG     // Debugging information
+}
